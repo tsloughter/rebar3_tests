646ac4516e431a1cc7b1e76496b14a155172d1d1
Tristan Sloughter <t@crashfast.com>
2018-11-30 15:15:29 -0700

    print rebar3 version in test script

--- run_tests.sh
+++ run_tests.sh
@@ -10,8 +10,11 @@ if [ "$1" = "ci" ]
 then
     curl -k https://s3.amazonaws.com/rebar3-nightly/rebar3 -o rebar3
     chmod +x rebar3
+    ./rebar3 version
     export PATH=$(dirname $(realpath $0)):$PATH
 fi
+
+rebar3 version
 
 find . -name _build | xargs rm -rf
 
